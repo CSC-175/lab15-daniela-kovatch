1427b37f2f3bea9a7c330a750d1b944c44a8777e
daniela-kovatch <dkovatch@umich.edu>
2025-03-30 19:06:31 -0400

    Test 1

--- lab15.cpp
+++ lab15.cpp
@@ -11,7 +11,27 @@ double factorial(int);
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 ********************************************************************/
+void getInfo(int &n, int &k) {
+    do {
+        cout << "How many balls (1-12) are in the pool to pick from? ";
+        cin >> n;
+        if (cin.fail() || n < 1 || n > 12) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Input Error! There must be between 1 and 12 balls.\n";
+        }
+    } while (n < 1 || n > 12);
 
+    do {
+        cout << "How many balls (1-" << n << ") will be drawn? ";
+        cin >> k;
+        if (cin.fail() || k < 1 || k > n) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Input Error! \n";
+        }
+    } while (k < 1 || k > n);
+}
 
 
 /*******************************************************************
@@ -25,11 +45,17 @@ double factorial(int);
 * multiplying two factorials together. This is done to prevent any *
 * intermediate result becoming so large that it causes overflow.   *
 ********************************************************************/
-
+double computeWays(int n, int k) {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+}
 
 /*******************************************************************
 * factorial                                                        *
 * This function computes factorials recursively.                   *
 *******************************************************************/
-
+double factorial(int num) {
+    if (num == 0 || num == 1)
+        return 1;
+    return num * factorial(num - 1);
+}
 
